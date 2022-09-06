a691d6f8e7a1af267a7ba073b80150925ae62e6f
William <wms274@nyu.edu>
2022-09-05 20:01:45 -0400

    commit tree 24f1ec5b3d07ecba8b68a72508951b030273589e

--- index.js
+++ index.js
@@ -10,7 +10,56 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
+let casedArray = []
 const titleCased = () => {
-  return tutorials
+  for (let x = 0; x<tutorials.length; x++) {
+    let words = tutorials[x].split(' ')
+
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].substr(1)
+    }
+
+    let newTutorialString = words.join(' ')
+
+    casedArray.push(newTutorialString)
+  }
+  return casedArray
 }
+
+
+// const titleCased = () => {
+//   tutorials.map(string => {
+//     for (letters of string) {
+//       let newString = ''
+//       if (letter === ' ') {
+//         let letterIndex = indexOf.letter()
+//         string[letterIndex + 1] = letter[letterIndex + 1].toUpperCase
+//       }
+//     }
+//     return `${toUpperCase.string[0]}`
+//   })
+//   return tutorials
+// }
+
+// const titleCased = () => {
+//   let array = []
+//   for (let x = 0; x < tutorials[0].length; x++) {
+//     if (tutorials[0][x] === ' ') {
+//       array.push(x+1)
+//     }
+//   }
+  
+//   // let newString = tutorials[0].slice(0, array[0]) + tutorials[0][array[0]].toUpperCase() + tutorials[0].slice(array[0]+1, tutorials[0].length)
+//   // console.log(newString)
+  
+//   function addSomeCaps() {
+//     let newString = tutorials[0]
+//     for (indexValue of array) {
+//       newString = tutorials[0].slice(0, indexValue) + tutorials[0][indexValue].toUpperCase() + tutorials[0].slice(indexValue+1, tutorials[0].length);
+//     }  
+//     console.log(newString);
+//   }
+//   addSomeCaps()
+  
+//   console.log(`${tutorials[0][0].toUpperCase()}${tutorials[0].slice(1)}`)
+// }
